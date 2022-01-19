ce48842600bf634f91df5e488bea62c44a484ddb
joshroche1 <joshua.a.roche@gmail.com>
2022-01-19 14:36:21 +0100

    Update HomeController.java

--- src/main/java/net/jar/Controllers/HomeController.java
+++ src/main/java/net/jar/Controllers/HomeController.java
@@ -1,4 +1,4 @@
-package net.jar.controller;
+package net.jar.Controllers;
 
 import java.io.IOException;
 
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-
+	
 	@RequestMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
 		return new ModelAndView("home");
