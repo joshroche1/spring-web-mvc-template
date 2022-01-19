e22dcfa753c4f128b52e1e87ffc8a4c4d62c862c
joshroche1 <joshua.a.roche@gmail.com>
2022-01-19 13:13:51 +0000

    build

--- src/main/java/net/jar/HelloController.java
+++ src/main/java/net/jar/HelloController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-  
+
   @RequestMapping("/")
   public String display() {
     return "index";
   }
-  
+
 }
